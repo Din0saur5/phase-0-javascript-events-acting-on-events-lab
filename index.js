a080e2cd0e0866723a2dd7924a43d1413abca119
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-14 18:25:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+
+document.addEventListener("keydown",function(event){
+    console.log(event);
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+});
+      
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    console.log(left)
+    if (left > 0 ){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    console.log(left)
+    if (left < 400 ){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
